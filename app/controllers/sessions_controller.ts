364ceda228f058b43944ae1862bad2b263d99465
artur-seppa <artur.reiman@gmail.com>
2025-10-24 16:44:11 -0300

    feat: create route sessions

---
+++ app/controllers/sessions_controller.ts
@@ -0,0 +1,32 @@
+import type { HttpContext } from '@adonisjs/core/http'
+import {
+    createSessionValidator,
+} from '#validators/session'
+import hash from '@adonisjs/core/services/hash'
+import Account from '#models/account'
+
+export default class SessionsController {
+    async login({ request, response }: HttpContext) {
+        const payload = await request.validateUsing(createSessionValidator);
+
+        try {
+            const account = await Account.findBy('email', payload.email);
+
+            if (!account) {
+                return response.badRequest({ message: 'Invalid credentials' });
+            }
+
+            const isPasswordValid = await hash.verify(account.password, payload.password);
+
+            if (!isPasswordValid) {
+                return response.badRequest({ message: 'Invalid credentials' });
+            }
+
+            const token = await Account.accessTokens.create(account);
+
+            return response.ok(token);
+        } catch (error) {
+            return response.internalServerError({ message: 'Internal server error' });
+        }
+    }
+}
--- start/routes.ts
+++ start/routes.ts
@@ -9,5 +9,7 @@
 
 import router from '@adonisjs/core/services/router'
 const AccountsController = () => import('#controllers/accounts_controller')
+const SessionsController = () => import('#controllers/sessions_controller')
 
 router.post('/accounts', [AccountsController, 'create']);
+router.post('/sessions', [SessionsController, 'login']);
